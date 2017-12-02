70e541d0154eca19d239d140db24a330571a625e
14bposad <14bposad@gmail.com>
2017-12-02 00:39:56 -0500

    Fixing merge conflicts

---
+++ app/src/main/java/com/apatel428/a69registration/activities/MyDataSetGet.java
@@ -0,0 +1,17 @@
+package com.apatel428.a69registration.activities;
+
+/**
+ * Created by 14bpo on 12/1/2017.
+ */
+
+public class MyDataSetGet {
+    private String createdDate;
+
+    public String getHello(){
+        return createdDate;
+    }
+
+    public void setHello(){
+        this.createdDate = createdDate;
+    }
+}
